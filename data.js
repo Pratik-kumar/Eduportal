96e353bbe315f54ff4ee7824ec90ba84660b9092
Pratik-kumar <pratik.kumarpsc@gmail.com>
2016-10-04 00:44:43 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,79 @@
+var learningData =
+{
+    "paths": [{
+        "id": "1",
+        "name": "User Experience Design",
+        "image": "",
+        "tags": "design research, UI frameworks, wireframes, user centric approach",
+        "learner": "26,093",
+        "hours": "131+",
+        "description": "This Learning Path is a curriculum of UX Design courses, videos and resources from across the internet, organized into a logical sequence that a beginner can follow.",
+        "sign_up": ""
+    }, {
+        "id": "2",
+        "name": "MBA Essentials",
+        "image": "",
+        "tags": "finance, accounting, opertions, strategy",
+        "learner": "15,566",
+        "hours": "505+",
+        "description": "Learn core business skills and how to apply them professionally and in your personal life",
+        "sign_up": ""
+    }, {
+        "id": "3",
+        "name": "Window App Dev",
+        "image": "",
+        "tags": "material design, sensors, maps, location service, studio",
+        "learner": "22,887",
+        "hours": "91+",
+        "description": "Learn Window 10 programming by building apps from scratch. Learn how to design and build Win apps and take your ideas to millions of people.",
+        "sign_up": ""
+    }, {
+        "id": "4",
+        "name": "Social Entrepreneurship",
+        "image": "",
+        "tags": "design solution, business plan, failing fast, learning quick",
+        "learner": "1,715",
+        "hours": "17+",
+        "description": "A robust introduction to Social Entrepreneurship, Learn and build a roadmap to launch your very own social venture",
+        "sign_up": ""
+    }, {
+        "id": "5",
+        "name": "Apply to Y Combinator",
+        "image": "",
+        "tags": "startups, hackernews, ycombinator,",
+        "learner": "1,658",
+        "hours": "3+",
+        "description": "A set of resources from YC partners and alumni to help you turn in a strong YC application. This course is collection of Gautam Tambay, Founder, Springboard.",
+        "sign_up": ""
+    }, {
+        "id": "6",
+        "name": "Data Analysis",
+        "image": "",
+        "tags": "processing, wrangling, visualizations, prediction, analysys",
+        "learner": "34,068",
+        "hours": "310+",
+        "description": "Learn statistics, data wrangling and visualization with this free curriculum By an Airbnb\/MIT alum. Learn how to manipulate and analyze data better with this free online curriculum",
+        "sign_up": ""
+    }, {
+        "id": "7",
+        "name": "Backend: Py\/Django",
+        "image": "",
+        "tags": "python, django, backend",
+        "learner": "16,830",
+        "hours": "74+",
+        "description": "Learn how to build websites and web-apps from scratch using Python and Django. Build and deploy fully functional web applications. Become a full-stack developer with the help of one of the founder Paul.",
+        "sign_up": ""
+    },
+    {
+        "id": "8",
+        "name": "Design and analysis of Algorithm",
+        "image": "",
+        "tags": "Algorithm, Computer Science",
+        "learner": "50,000",
+        "hours": "131+",
+        "description": "This Learning Path is a curriculum of Algorith design and its effective implementation",
+        "sign_up": ""
+    }],
+    "quote_max": "100000",
+    "quote_available": "8379"
+};
